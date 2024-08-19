3eba64f468e6fe9fb9aed57055dc26781b18cba7
Magic <notarealemail@gmail.com>
2024-08-19 13:03:02 +0200

    add input for float64

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package keyboard
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func Hi() {
-	fmt.Println("hiihihihhi")
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, err
 }
